c968d73752d7400733476f97d1f6ccb6f17e5d3e
Yahor <yahordarashuk@gmail.com>
2025-10-18 16:08:30 +0300

    Fix config.py coordinator_address issue

--- src/config.py
+++ src/config.py
@@ -3,9 +3,9 @@ import os
 IS_CLIENT = os.getenv("IS_CLIENT")
 IS_SERVER = os.getenv("IS_SERVER")
 
-COORDINATOR_IP = os.getenv("COORDINATOR_IP")
-COORDINATOR_PORT = int(os.getenv("COORDINATOR_PORT"))
-
+COORDINATOR_IP = os.getenv("COORDINATOR_IP", "localhost")
+COORDINATOR_PORT = int(os.getenv("COORDINATOR_PORT", "8888"))
+coordinator_address = f"{COORDINATOR_IP}:{COORDINATOR_PORT}"
 
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
